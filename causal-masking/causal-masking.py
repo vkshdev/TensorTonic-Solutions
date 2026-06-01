2d36c81bed1de70aa2e2a49dc04e63bb284d12ce
Vikash Mehta <itsmevikashkr7@gmail.com>
2026-06-01 23:55:20 +0530

    Update solution for causal-masking

--- causal-masking/causal-masking.py
+++ causal-masking/causal-masking.py
@@ -11,14 +11,11 @@ def apply_causal_mask(scores, mask_value=-1e9):
     scores = np.array(scores, dtype=float)
     if scores.shape[-1] != scores.shape[-2]:
         raise ValueError("Last two dimensions must be equal (T,T)")
+        
     T = scores.shape[-1]
-    
     mask = np.triu(np.ones((T, T)), k=1).astype(bool)
-    
     masked_scores = scores.copy()
-    
     masked_scores[..., mask] = mask_value
-    
     return masked_scores
     
     pass
