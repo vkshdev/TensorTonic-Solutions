b974c08b546df65c23a4bcd90657b1246a3049c7
Vikash Mehta <itsmevikashkr7@gmail.com>
2026-06-07 10:33:14 +0530

    Solve nesterov-momentum

---
+++ nesterov-momentum/nesterov-momentum.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def nesterov_momentum_step(w, v, grad, lr=0.01, momentum=0.9):
+    """
+    Perform one Nesterov Momentum update step.
+    """
+    # Write code here
+
+    w = np.asarray(w, dtype=float)
+    v = np.asarray(v, dtype=float)
+    grad = np.asarray(grad, dtype=float)
+    new_v = momentum * v + lr * grad
+    new_w = w - new_v
+
+    return new_w, new_v
+    pass
